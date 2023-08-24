43da27211d371e7f1f6210a70c37ce6e3575d3fb
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-24 17:29:28 -0500

    Tercer ejercicio trabajando con una cadena de caracteres.

---
+++ src/CadenasDeCaracteres.java
@@ -0,0 +1,8 @@
+public class CadenasDeCaracteres {
+    public static void main(String[] args) {
+        String nombre = "Daniel";
+        char segundaLetra = nombre.charAt(1);
+        System.out.println("La segunda letra del nombre es " + segundaLetra);
+        System.out.println("El nombre tiene " + nombre.length() + " letras");
+    }
+}
